cae547c47aa02043aac8451b9ae8c013166ffbeb
Alex Santillan <asantill68@yahoo.com>
2020-03-01 22:25:48 -0600

    Addes some front end work

--- routes/api-html.js
+++ routes/api-html.js
@@ -1,15 +1,16 @@
 //  HTML Routes
+var path = require("path");
 
 module.exports = function(app) {
     //  At the root path, send a homepagesimple hello world mesaage to the browser
-    app.get("/", function(req, res) {
-        res.send("views/");
-    });
+    // app.get("/profile", function(req, res) {
+    //     res.sendFile(path.join(__dirname, "../views/profile.html");
+    // });
 
     //  Send Profile at the /profile
-    // app.get("/profile", function(req, res) {
-    //     res.sendFile(path.join(__dirname + "views/profile.html"))
-    // });
+    app.get("/profile", function(req, res) {
+        res.sendFile(path.join(__dirname + "/views/profile.html"))
+    });
 };
 
 
