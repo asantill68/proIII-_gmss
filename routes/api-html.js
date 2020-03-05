400861593d7e68ab7cf375a8edb7afe54c2e3c4b
Alex Santillan <asantill68@yahoo.com>
2020-03-04 22:08:12 -0600

    Finished HTML

--- routes/api-html.js
+++ routes/api-html.js
@@ -12,12 +12,12 @@ module.exports = function(app) {
         res.sendFile(path.join(__dirname + "/../views/createProfile.html"))
     });
     //  Send Profile at the /profile
-    app.get("/Questionaire1", function(req, res) {
-        res.sendFile(path.join(__dirname + "/../views/questionaire1.html"))
+    app.get("/Quest1", function(req, res) {
+        res.sendFile(path.join(__dirname + "/../views/quest1.html"))
     });
     //  Send Profile at the /profile
-    app.get("/Questionaire2", function(req, res) {
-        res.sendFile(path.join(__dirname + "/../views/questionaire2.html"))
+    app.get("/Quest2", function(req, res) {
+        res.sendFile(path.join(__dirname + "/../views/quest2.html"))
     });
     //  Send Profile at the /profile
     app.get("/FAQ", function(req, res) {
